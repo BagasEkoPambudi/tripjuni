4d8b6cd6cd97f501c5861b1c172f25b79066af8f
Bagas Eko Pambudi <104248538+BagasEkoPambudi@users.noreply.github.com>
2026-05-30 15:00:01 +0700

    Delete trip-bot directory

--- trip-bot/cek_model.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AQ.Ab8RN6KI8z5RPXFNfnXBrSpBgTlPv_9AEDGvYNt71VNebWi_eQ")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
